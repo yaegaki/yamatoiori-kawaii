62e3a1106bb72e14abb92c0fd2305f3bb9f5a6ef
yaegaki <yaegaki.fino@gmail.com>
2019-01-06 22:44:10 +0900

    既にローカルにある情報は取得しない処理を追加

--- scripts/get_video_info.py
+++ scripts/get_video_info.py
@@ -67,9 +67,13 @@ if not os.path.isdir(output_dir):
     os.makedirs(output_dir)
 
 
-print([{x['id']: x} for x in get_local_video_info()])
+
+local_dict = {x['id']: x for x in get_local_video_info()}
 ids = get_all_video_ids()
 for id in ids:
+    if id in local_dict:
+        print('{} skipped.'.format(id))
+        continue
     time.sleep(1)
     summary = get_video_summary(id)
     s = json.dumps(summary, ensure_ascii=False)

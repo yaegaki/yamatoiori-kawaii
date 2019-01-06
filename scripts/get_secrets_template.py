aae163139136d30d364a65a1222a978523f38ed2
yaegaki <yaegaki.fino@gmail.com>
2019-01-06 23:46:06 +0900

    キー関連はignoreしたファイルから取得するように変更

---
+++ scripts/get_secrets_template.py
@@ -0,0 +1,7 @@
+# YoutubeDataAPIにアクセスするためのキー
+def youtube_developer_key():
+    return 'XXXX'
+
+# アプリにログインしたときのクッキー
+def app_login_cookie():
+    return 'XXXX'
--- scripts/get_video_info.py
+++ scripts/get_video_info.py
@@ -8,13 +8,14 @@ import time
 import os
 
 from get_local_video_info import get_local_video_info
+import get_secrets
 
 
 # Set DEVELOPER_KEY to the API key value from the APIs & auth > Registered apps
 # tab of
 #   https://cloud.google.com/console
 # Please ensure that you have enabled the YouTube Data API for your project.
-DEVELOPER_KEY = "XXXXXXXXX"
+DEVELOPER_KEY = get_secrets.youtube_developer_key()
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 

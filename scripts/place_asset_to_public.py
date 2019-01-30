3980d175fbe1ba56496d51674d85ebe13656c95d
yaegaki <yaegaki.fino@gmail.com>
2019-01-30 20:57:30 +0900

    publicとbuildの両方に配置するように変更

--- scripts/place_asset_to_public.py
+++ scripts/place_asset_to_public.py
@@ -5,32 +5,36 @@ import shutil
 
 from get_local_video_info import get_local_video_info
 
-work_dir = './work'
-
-output_base_dir = './web/public/asset/video'
+output_base_dirs = ['./web/public/asset/video', './web/build/asset/video']
 
 for info in get_local_video_info():
     id = info['id']
     exists_chat_data = info['existsChatData']
+    place_any = False
 
-    output_dir = '{}/{}'.format(output_base_dir, id)
-    if not os.path.isdir(output_dir):
-        os.makedirs(output_dir)
-    
-    if exists_chat_data:
-        # 集計したデータをinfoにねじ込む
-        with codecs.open('./video_src/{}/aggregate.json'.format(id), 'r', 'utf-8') as f:
-            info['words'] = json.loads(f.read())
+    for output_base_dir in output_base_dirs:
+        output_dir = '{}/{}'.format(output_base_dir, id)
+        if not os.path.isdir(output_dir):
+            os.makedirs(output_dir)
+            place_any = True
         
-        wc_dst_path = '{}/wc.png'.format(output_dir)
-        if not os.path.exists(wc_dst_path):
-            # ワードクラウドのコピー
-            shutil.copyfile('./video_src/{}/wc.png'.format(id), wc_dst_path)
-    
-    detail_path = '{}/detail.json'.format(output_dir)
-    if not os.path.exists(detail_path):
-        with codecs.open(detail_path, 'w', 'utf-8') as f:
-            f.write(json.dumps(info, ensure_ascii=False))
-    print('{} done.'.format(id))
+        if exists_chat_data:
+            # 集計したデータをinfoにねじ込む
+            if 'words' not in info:
+                with codecs.open('./video_src/{}/aggregate.json'.format(id), 'r', 'utf-8') as f:
+                    info['words'] = json.loads(f.read())
+            
+            wc_dst_path = '{}/wc.png'.format(output_dir)
+            if not os.path.exists(wc_dst_path):
+                # ワードクラウドのコピー
+                shutil.copyfile('./video_src/{}/wc.png'.format(id), wc_dst_path)
+                place_any = True
+        
+        detail_path = '{}/detail.json'.format(output_dir)
+        if not os.path.exists(detail_path):
+            with codecs.open(detail_path, 'w', 'utf-8') as f:
+                f.write(json.dumps(info, ensure_ascii=False))
+            place_any = True
 
-
+    if place_any:
+        print('{} done.'.format(id))
